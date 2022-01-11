415491abb05d79376bb44dfa0d16c2e08605be7f
anderskate <andrey.starozhilov@mail.ru>
2022-01-11 22:16:39 +0500

    add SMS status check

--- smsc_api.py
+++ smsc_api.py
@@ -5,12 +5,14 @@ from dataclasses import dataclass, asdict
 import asks
 import asyncclick as click
 from dotenv import load_dotenv
+from loguru import logger
 
 
 load_dotenv()
 
 
-SMSC_API_URL = 'https://smsc.ru/sys/send.php'
+SMSC_SENDING_URL = 'https://smsc.ru/sys/send.php'
+SMSC_STATUS_URL = 'https://smsc.ru/sys/status.php'
 
 
 @dataclass
@@ -43,13 +45,25 @@ async def send_sms_message(**kwargs):
     sms_lifetime = kwargs.get('lifetime')
     sms_info = SmsMessageInfo(
         login=os.getenv('SMSC_LOGIN'), psw=os.getenv('SMSC_PASSWORD'),
-        sender=os.getenv('SMSC_SENDER'), mes=message, phones=phones,
+        sender=os.getenv('SMSC_SENDER'), mes=message, phones=phones
     )
     if sms_lifetime:
         sms_info.valid = sms_lifetime
     encoded_sms_info = urllib.parse.urlencode(asdict(sms_info))
-    response = await asks.get(SMSC_API_URL, params=encoded_sms_info)
-    print(response.json())
+    response = await asks.get(SMSC_SENDING_URL, params=encoded_sms_info)
+    logger.info('Message sent')
+
+    sms_id = response.json().get('id')
+
+    info_for_getting_sms_status = {
+        'login': sms_info.login, 'psw': sms_info.psw,
+        'phone': sms_info.phones, 'id': sms_id, 'fmt': sms_info.fmt,
+    }
+    encoded_status_info = urllib.parse.urlencode(info_for_getting_sms_status)
+    status_response = await asks.get(
+        SMSC_STATUS_URL, params=encoded_status_info)
+
+    logger.info(f'Message status - {status_response.json()}')
 
 
 if __name__ == '__main__':
