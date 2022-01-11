b6c4313e8846f5024bff680a9c8c45477ccc507d
anderskate <andrey.starozhilov@mail.ru>
2022-01-11 21:42:30 +0500

    Add ability to send sms throw smsc api

---
+++ smsc_api.py
@@ -0,0 +1,49 @@
+import os
+import urllib.parse
+from dataclasses import dataclass, asdict
+
+import asks
+import asyncclick as click
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+SMSC_API_URL = 'https://smsc.ru/sys/send.php'
+
+
+@dataclass
+class SmsMessageInfo:
+    """Sms message info for sending throw smsc.ru api."""
+    login: str
+    psw: str
+    phones: str
+    mes: str
+    sender: str
+    fmt: int = 3
+    valid: int = 1
+
+
+@click.command()
+@click.option(
+    "--phones",
+    help="Comma-separated phone numbers where SMS will be sent",
+    required=True,
+)
+@click.option("--message", default='Hello World!', help="Message for sending")
+async def send_sms_message(**kwargs):
+    phones = kwargs.get('phones')
+    message = kwargs.get('message')
+    sms_info = SmsMessageInfo(
+        login=os.getenv('SMSC_LOGIN'), psw=os.getenv('SMSC_PASSWORD'),
+        sender=os.getenv('SMSC_SENDER'), mes=message, phones=phones,
+    )
+    encoded_sms_info = urllib.parse.urlencode(asdict(sms_info))
+    response = await asks.get(SMSC_API_URL, params=encoded_sms_info)
+    print(response.json())
+
+
+if __name__ == '__main__':
+    send_sms_message(_anyio_backend="trio")
+
